f54b404377b5d70da0aa5bc72426c0f62ef67f74
jas1233 <136487510+jas1233@users.noreply.github.com>
2025-03-26 05:49:02 +0530

    Update route.ts

--- memorial-backend/api/memorials/route.ts
+++ memorial-backend/api/memorials/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import connectToDatabase from "@/lib/mongodb";
+import connectToDatabase from "memorial-backend/lib/mongodb.ts";
 import Memorial from "@/models/memorial";
 
 export async function GET() {
