1d2c7d6ab2ba1c3ec7894ecb0e41139175153096
jas1233 <136487510+jas1233@users.noreply.github.com>
2025-03-26 05:41:57 +0530

    Update memorial.ts

--- memorial-backend/models/memorial.ts
+++ memorial-backend/models/memorial.ts
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
+
+const MemorialSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    dob: { type: String, required: true },
+    dod: { type: String, required: true },
+    message: { type: String, required: true },
+    image: { type: String, required: false },
+}, { timestamps: true });
+
+const Memorial = mongoose.models.Memorial || mongoose.model("Memorial", MemorialSchema);
+export default Memorial;
 
