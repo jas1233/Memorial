ceafac7bb34ae0af775f5a2b18cc644ce19ecece
jas1233 <136487510+jas1233@users.noreply.github.com>
2025-03-26 05:58:40 +0530

    Update mongodb.ts

--- memorial-backend/lib/mongodb.ts
+++ memorial-backend/lib/mongodb.ts
@@ -1,11 +1,24 @@
-import mongoose from "mongoose";
+import { MongoClient } from 'mongodb';
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongo:WjczkLCqtidEeEUTZpOVGAkRSmvcVEuV@mongodb.railway.internal:27017";
+const uri = process.env.MONGODB_URI || '';
+const options = {};
 
-const connectToDatabase = async () => {
-    if (mongoose.connection.readyState >= 1) return;
-    await mongoose.connect(MONGODB_URI, { dbName: "memorial" });
-};
+let client;
+let clientPromise: Promise<MongoClient>;
 
-export default connectToDatabase;
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
 
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
